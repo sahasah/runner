f3f98e2d6f4984591cc821f5cca02fa44c43c4dd
saharat <saharat27150@gmail.com>
2019-06-20 14:11:10 +0700

    runner
    runner

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1561014327,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/sprite3-sheet0.png",
+		"images/tiledbackground4.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
